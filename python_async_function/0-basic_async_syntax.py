ac1e3969a6a935aa1f68ad2f60c085deb42a6b54
nicolas <nicolas.quoirez@hotmail.fr>
2024-04-24 12:05:11 +0200

    wrinting functions to file 0 and 1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""This module defines the async function <wait_random()>."""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous coroutine that takes an 'int' as argument.
+    Waits for a random delay(0 and <max_delay>).
+    Returns it.
+
+    Args:
+        max_delay (int): Maximum timeout limit. Defaults to 10.
+
+    Returns:
+        random_delay (float): Got using the module random.
+    """
+    random_delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""This module defines the async function <wait_n()>."""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Async routine, that takes 2 'int' arguments.
+    Spawn <wait_random()> <n> times with the specified <max_delay>.
+    return the list of all the delays, in ascending order.
+
+    Args:
+        n (int): Number of iteration.
+        max_delay (int): Maximum timeout limit.
+
+    Returns:
+        List[float]: List of all delay created, in ascending order.
+    """
+    list_random_delay = list()
+    # On each iteration, generate random async task, and adds it at the list.
+    for i in range(n):
+        list_random_delay.append(wait_random(max_delay))
+
+    # Return a list of each async task completed, in ascending order.
+    return [await delay for delay in asyncio.as_completed(list_random_delay)]
