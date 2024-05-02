1243019ee45cd4a2ada5cc6543515783ceb59222
nicolas <nicolas.quoirez@hotmail.fr>
2024-05-02 12:36:48 +0200

    save

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module defines the functions 'index_range()'. """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    pass
